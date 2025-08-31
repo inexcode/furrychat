7ab558e7aaf269beada22cb51ec83c4f7c91bf29
Inex Code <gitea@inex.rocks>
2025-08-31 17:08:02 +0300

    fix: Phantom paddings on Android 16

--- lib/widgets/layouts/two_column_layout.dart
+++ lib/widgets/layouts/two_column_layout.dart
@@ -17,24 +17,28 @@ class TwoColumnLayout extends StatelessWidget {
 
     return ScaffoldMessenger(
       child: Scaffold(
-        body: Row(
-          children: [
-            Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(),
-              width: FluffyThemes.columnWidth + FluffyThemes.navRailWidth,
-              child: mainView,
-            ),
-            Container(
-              width: 1.0,
-              color: theme.dividerColor,
-            ),
-            Expanded(
-              child: ClipRRect(
-                child: sideView,
+        body: SafeArea(
+          bottom: false,
+          top: false,
+          child: Row(
+            children: [
+              Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(),
+                width: FluffyThemes.columnWidth + FluffyThemes.navRailWidth,
+                child: mainView,
               ),
-            ),
-          ],
+              Container(
+                width: 1.0,
+                color: theme.dividerColor,
+              ),
+              Expanded(
+                child: ClipRRect(
+                  child: sideView,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
